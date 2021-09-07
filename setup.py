b1698e8c08875acb223e095fa3cfe610c9ad285b
Marcel Robeer <marcel_robeer@hotmail.com>
2021-09-07 15:11:49 +0200

    Fix in dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding
 
 setuptools.setup( # type: ignore
     name = 'text_explainability',
-    version = '0.3.6',
+    version = '0.3.7',
     description = 'Generic explainability architecture for text machine learning models',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
@@ -23,7 +23,7 @@ setuptools.setup( # type: ignore
     packages = setuptools.find_packages(), # type : ignore
     install_requires = [
         'instancelib>=0.3.2.0',
-        'numpy>=1.19.5'
+        'numpy>=1.19.5',
         'python-i18n>=0.3.9',
         'scikit-learn>=0.24.1',
     ],
