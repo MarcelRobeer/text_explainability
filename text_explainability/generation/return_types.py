94b10c413e1a127f0a3beebe9a6ea9b5d83a087c
Marcel Robeer <marcel_robeer@hotmail.com>
2021-09-16 13:14:42 +0200

    ReturnType refactoring

--- text_explainability/generation/return_types.py
+++ text_explainability/generation/return_types.py
@@ -10,28 +10,25 @@ import numpy as np
 from typing import Union, Optional, Sequence, Dict, Tuple
 from instancelib import InstanceProvider
 
+from text_explainability.generation.surrogate import TreeSurrogate
 
-class FeatureList:
+
+class BaseReturnType:
     def __init__(self,
                  used_features: Union[Sequence[str], Sequence[int]],
-                 scores: Union[Sequence[int], Sequence[float]],
                  labels: Optional[Sequence[int]] = None,
                  labelset: Optional[Sequence[str]] = None):
-        """Save scores per feature, grouped per label.
-
-        Examples of scores are feature importance scores, or counts of features in a dataset.
+        """Base return type.
 
         Args:
             used_features (Union[Sequence[str], Sequence[int]]): Used features per label.
-            scores (Union[Sequence[int], Sequence[float]]): Scores per label.
             labels (Optional[Sequence[int]], optional): Label indices to include, if none provided 
                 defaults to 'all'. Defaults to None.
             labelset (Optional[Sequence[str]], optional): Lookup for label names. Defaults to None.
         """
         self._used_features = used_features
-        self._labelset = labelset
         self._labels = labels
-        self._scores = scores
+        self._labelset = labelset
 
     @property
     def labels(self):
@@ -66,6 +63,33 @@ class FeatureList:
         if self.labelset is not None:
             return self.labelset[idx]
         return idx
+
+    def __repr__(self) -> str:
+        labels = [self.label_by_index(label) for label in self.labels] if self.labels is not None else None
+        return f'{self.__class__.__name__}(labels={labels}, used_features={self.used_features})'
+
+
+class FeatureList(BaseReturnType):
+    def __init__(self,
+                 used_features: Union[Sequence[str], Sequence[int]],
+                 scores: Union[Sequence[int], Sequence[float]],
+                 labels: Optional[Sequence[int]] = None,
+                 labelset: Optional[Sequence[str]] = None):
+        """Save scores per feature, grouped per label.
+
+        Examples of scores are feature importance scores, or counts of features in a dataset.
+
+        Args:
+            used_features (Union[Sequence[str], Sequence[int]]): Used features per label.
+            scores (Union[Sequence[int], Sequence[float]]): Scores per label.
+            labels (Optional[Sequence[int]], optional): Label indices to include, if none provided 
+                defaults to 'all'. Defaults to None.
+            labelset (Optional[Sequence[str]], optional): Lookup for label names. Defaults to None.
+        """
+        super().__init__(used_features=used_features,
+                         labels=labels,
+                         labelset=labelset)
+        self._scores = scores
 
     def get_raw_scores(self, normalize: bool = False) -> np.ndarray:
         """Get saved scores per label as `np.ndarray`.
@@ -117,12 +141,45 @@ class FeatureList:
     def __str__(self) -> str:
         return '\n'.join([f'{a}: {str(b)}' for a, b in self.scores.items()])
 
-    def __repr__(self) -> str:
-        labels = [self.label_by_index(label) for label in self.labels] if self.labels is not None else None
-        return f'{self.__class__.__name__}(labels={labels}, used_features={self.used_features})'
+
+class DataExplanation:
+    def __init__(self,
+                 provider: InstanceProvider,
+                 sampled: bool = False):
+        """Save the sampled/generated instances used to determine an explanation.
+
+        Args:
+            provider (InstanceProvider): Sampled or generated data, including original instance.
+            sampled (bool, optional): Whether the data in the provider was sampled (True) or generated (False). 
+                Defaults to False.
+        """
+        self._provider = provider
+        self._original_instance = self._provider[next(iter(self._provider))]
+        self._neighborhood_instances = self._provider.get_children(self._original_instance)
+        self.sampled = sampled
+
+    @property
+    def original_instance(self):
+        """The instance for which the feature attribution scores were calculated."""
+        return self._original_instance
+
+    @property
+    def perturbed_instances(self):
+        """Perturbed versions of the original instance, if `sampled=False` during initialization."""
+        return None if self.sampled else self._neighborhood_instances
+
+    @property
+    def sampled_instances(self):
+        """Sampled instances, if `sampled=True` during initialization."""
+        return self._neighborhood_instances if self.sampled else None
+
+    @property
+    def neighborhood_instances(self):
+        """Instances in the neighborhood (either sampled or perturbed)."""
+        return self._neighborhood_instances
 
 
-class FeatureAttribution(FeatureList):
+class FeatureAttribution(FeatureList, DataExplanation):
     def __init__(self,
                  provider: InstanceProvider,
                  used_features: Union[Sequence[str], Sequence[int]],
@@ -149,36 +206,14 @@ class FeatureAttribution(FeatureList):
             sampled (bool, optional): Whether the data in the provider was sampled (True) or generated (False). 
                 Defaults to False.
         """
-        super().__init__(used_features=used_features,
-                         scores=scores,
-                         labels=labels,
-                         labelset=labelset)
-        self._provider = provider
+        super(self, FeatureList).__init__(used_features=used_features,
+                                          scores=scores,
+                                          labels=labels,
+                                          labelset=labelset)
+        super(self, DataExplanation).__init__(provider=provider,
+                                              sampled=sampled)
         self._base_score = base_score
         self._scores_stddev = scores_stddev
-        self._original_instance = self._provider[next(iter(self._provider))]
-        self._neighborhood_instances = self._provider.get_children(self._original_instance)
-        self.sampled = sampled
-
-    @property
-    def original_instance(self):
-        """The instance for which the feature attribution scores were calculated."""
-        return self._original_instance
-
-    @property
-    def perturbed_instances(self):
-        """Perturbed versions of the original instance, if `sampled=False` during initialization."""
-        return None if self.sampled else self._neighborhood_instances
-
-    @property
-    def sampled_instances(self):
-        """Sampled instances, if `sampled=True` during initialization."""
-        return self._neighborhood_instances if self.sampled else None
-
-    @property
-    def neighborhood_instances(self):
-        """Instances in the neighborhood (either sampled or perturbed)."""
-        return self._neighborhood_instances
 
     @property
     def used_features(self):
@@ -198,3 +233,51 @@ class FeatureAttribution(FeatureList):
         labels = [self.label_by_index(label) for label in self.labels] if self.labels is not None else None
         return f'{self.__class__.__name__}(labels={labels}, ' + \
             f'used_features={self.used_features}, n_{sampled_or_perturbed}_instances={n})'
+
+
+class Rules(BaseReturnType, DataExplanation):
+    def __init__(self,
+                 provider: InstanceProvider,
+                 used_features: Union[Sequence[str], Sequence[int]],
+                 rules: Union[Sequence[str], TreeSurrogate],
+                 labels: Optional[Sequence[int]] = None,
+                 labelset: Optional[Sequence[str]] = None,
+                 sampled: bool = False):
+        """Base return type.
+
+        Args:
+            used_features (Union[Sequence[str], Sequence[int]]): Used features per label.
+            rules (Union[Sequence[str], TreeSurrogate]): Rules applicable.
+            labels (Optional[Sequence[int]], optional): Label indices to include, if none provided 
+                defaults to 'all'. Defaults to None.
+            labelset (Optional[Sequence[str]], optional): Lookup for label names. Defaults to None.
+            sampled (bool, optional): Whether the data in the provider was sampled (True) or generated (False). 
+                Defaults to False.
+        """
+        super(self, BaseReturnType).__init__(used_features=used_features,
+                                             labels=labels,
+                                             labelset=labelset)
+        super(self, DataExplanation).__init__(provider=provider,
+                                              sampled=sampled)
+        self._extract_rules(rules)
+
+    @property
+    def used_features(self):
+        """Names of features of the original instance."""
+        if hasattr(self.original_instance, 'tokenized'):
+            return [self.original_instance.tokenized[i] for i in self._used_features]
+        return list(self._used_features)
+
+    def __repr__(self) -> str:
+        sampled_or_perturbed = 'sampled' if self.sampled else 'perturbed'
+        n = sum(1 for _ in self.neighborhood_instances)
+        labels = [self.label_by_index(label) for label in self.labels] if self.labels is not None else None
+        return f'{self.__class__.__name__}(labels={labels}, ' + \
+            f'used_features={self.used_features}, n_{sampled_or_perturbed}_instances={n})'
+
+    def _extract_rules(self, rules: Union[Sequence[str], TreeSurrogate]):
+        print(rules)
+        raise NotImplementedError('TODO: Convert tree to list of rules')
+
+    def rules(self):
+        raise NotImplementedError('TODO: Determine applicable rules to sample')
